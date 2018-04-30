55ed1906b6d884d5b6dcea6e67bbbaabfe64685b
Sanskriti Rathi <sanskriti@lawn-143-215-51-152.lawn.gatech.edu>
2018-04-30 15:17:38 -0400

    Implemented password reset
    User can reset their password using a link sent to their email

--- app/src/main/java/com/a2340/shelterhelper/LoginActivity.java
+++ app/src/main/java/com/a2340/shelterhelper/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -39,14 +40,40 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         Button loginBtn = findViewById(R.id.btn_login);
         loginBtn.setOnClickListener(this);
+        Button forgotPasswordBtn = findViewById(R.id.btn_forgot_password);
         //EditText userBox = (EditText) findViewById(R.id.tb_username);
 
         mAuth = FirebaseAuth.getInstance();
 
-
         LocalUsers.readInUsers(this);
+
+        forgotPasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userBox = findViewById(R.id.tb_username);
+                String username = userBox.getText().toString();
+
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(getApplicationContext(), "Please enter your email!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.sendPasswordResetEmail(username)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Check email to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Failed to send reset password email. PLease try again!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 
     @Override
